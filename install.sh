557e028fa451605bf3c42897fa3a8a83ac8e4675
raysuo <eduardo@rairan.com.br>
2025-08-31 14:34:50 +0200

    added waybar reload script and keybinds list

--- install.sh
+++ install.sh
@@ -27,3 +27,7 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 
+
+# Relaunch waybar
+killall -9 waybar
+waybar &
